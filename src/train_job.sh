e4c081c2dcaea6ef830f79b0928deede545305d1
Srivathsan Shanmuganathan <sshanmug@cedar5.cedar.computecanada.ca>
2023-04-10 16:50:28 -0700

    Change data directory and update script file

--- src/train_job.sh
+++ src/train_job.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --time=03:00:00
+#SBATCH --time=03-00
 #SBATCH --account=def-punithak
-#SBATCH --mem=32g
+#SBATCH --mem=64g
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=16
@@ -11,7 +11,7 @@ echo 'starting job'
 START_TIME=$(date +%s)
 source ../env/bin/activate
 echo $(python --version)
-python DQN.py --task train --files data/filenames/image_files.txt data/filenames/landmark_files.txt --model_name CommNet --file_type brain --landmarks 0 --multiscale --viz 0 --train_freq 50 --write --memory_size 32000 --init_memory_size 24000
+python DQN.py --task train --files data/echoFusion/filenames/image_files.txt data/echoFusion/filenames/landmark_files.txt --model_name CommNet --file_type fetal --landmarks 0 0 0 1 1 1 --multiscale --viz 0 --train_freq 50 --write --max_episodes 50000 --memory_size 64000 --init_memory_size 30000
 echo 'finished job'
 END_TIME=$(date +%s)
 echo "It took $(($END_TIME - $START_TIME)) seconds"
