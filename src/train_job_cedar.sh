f3b011775802d65e0982423c7ca0d24fc8b98584
Srivathsan Shanmuganathan <sshanmug@cedar5.cedar.computecanada.ca>
2023-04-22 11:51:18 -0700

    Cedar script

--- src/train_job_cedar.sh
+++ src/train_job_cedar.sh
@@ -11,7 +11,7 @@ echo 'starting job'
 START_TIME=$(date +%s)
 source ../env/bin/activate
 echo $(python --version)
-python DQN.py --task train --files data/echoFusion/filenames/image_files.txt data/echoFusion/filenames/landmark_files.txt --model_name CommNet --file_type fetal --landmarks 0 0 0 1 1 1 2 2 2 --multiscale --viz 0 --train_freq 50 --write --max_episodes 40000 --memory_size 100000 --init_memory_size 30000
+python DQN.py --task train --files data/echoFusion/filenames/image_files.txt data/echoFusion/filenames/landmark_files.txt --model_name CommNet --file_type fetal --landmarks 0 1 2 --multiscale --viz 0 --train_freq 50 --write --max_episodes 50000 --memory_size 100000 --init_memory_size 30000
 echo 'finished job'
 END_TIME=$(date +%s)
 echo "It took $(($END_TIME - $START_TIME)) seconds"
